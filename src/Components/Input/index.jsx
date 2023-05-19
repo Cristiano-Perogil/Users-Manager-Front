15ddc58b5a72f9d1b5208e58ea566d5c747f9057
Cristiano Perogil <cristianojuniorcol@gmail.com>
2023-05-19 12:10:51 -0300

    Fixed index js input component file conflict

--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
 
-function Input(props) {
-    const id = `${props.label.slice(0, 2)}ID`;
-
-    const [isNumber, setIsNumber] = useState(false);
-
-
-
-    return (
-        <div>
-            <label for={id}>{props.label}</label><br>
-            </br>
-            <input
-                type={isNumber ? "number" : "text"}
-                id={id}
-                value={props.value}
-                onChange={props.change}
-            />
-            {props.invalid &&
-                <div className="warn-aria-form">
-                    <i className='fas fa-exclamation-circle'></i>
-                    <span>The {props.label} cannot be empty.</span>
-                </div>
-            }
-        </div>
-    )
-}
-
-export default Input
-=======
 export { default } from "./Input";
->>>>>>> fix.inputFocus
+
